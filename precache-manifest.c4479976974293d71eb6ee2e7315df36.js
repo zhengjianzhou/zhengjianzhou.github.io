007bb8ef50eb39f8514153e440190d69ae92f893
James Zheng <jzzheng@gmail.com>
2020-10-22 15:32:50 +0800

    Add files via upload

---
+++ precache-manifest.c4479976974293d71eb6ee2e7315df36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0e47fff5d1fc5ffebe3429f976dc22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941fa9f46745d6ce11f1",
+    "url": "/static/css/main.62927a30.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941fa9f46745d6ce11f1",
+    "url": "/static/js/main.afb5491e.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
